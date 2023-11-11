44d2b24dfa9d662cf3e34a3849fa67250c229165
jakepreis <150413105+jakepreis@users.noreply.github.com>
2023-11-11 12:24:39 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,5 +2,5 @@ const boxPlayer = new BoxAudioPlayer("container___main");
 
 // Example: Add audio tracks to the container
 boxPlayer.addAudio("track1.mp3", "track2.mp3",'Boombap','Mikrofon za 200 PLN');
-boxPlayer.addAudio("track1.mp3", "track2.mp3",'Autotune','Mikrofon za 999 PLN');
-boxPlayer.addAudio("track1.mp3", "track2.mp3",'add you heading','add your subtitle');
+boxPlayer.addAudio("track3.mp3", "track4.mp3",'Autotune','Mikrofon za 999 PLN');
+boxPlayer.addAudio("track5.mp3", "track6.mp3",'Breakcore','Mikrofon za 300 PLN');
