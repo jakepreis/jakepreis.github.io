c8cadd981f7de160cee1802925178f0d9751273e
jakepreis <150413105+jakepreis@users.noreply.github.com>
2023-11-11 11:28:06 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const boxPlayer = new BoxAudioPlayer("container___main");
 
 // Example: Add audio tracks to the container
-boxPlayer.addAudio("track1.mp3", "track2.mp3",'Mikrofon za 100 złotych','Zgubione dzieciaki robią papier');
+boxPlayer.addAudio("track1.mp3", "track2.mp3",'Zgubione dzieciaki robią papier','Mikrofon za 100 PLN');
 boxPlayer.addAudio("track3.mp3", "track4.mp3",'Mikrofon za 1000 złotych','Tańczę breakdance do smutnych numerów');
 boxPlayer.addAudio("track1.mp3", "track2.mp3",'add you heading','add your subtitle');
 
